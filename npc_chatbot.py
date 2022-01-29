8c683019328cefbe30323885a01f090bbab55134
Thomas Butler <tombutler4@hotmail.com>
2022-01-28 20:36:06 -0600

    added backstory

--- npc_chatbot.py
+++ npc_chatbot.py
@@ -5,18 +5,21 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 start_sequence = "\nNPC:"
 restart_sequence = "\nAdventurer: "
-prompt = "The following is a conversation between an Adventurer and an NPC.\n"
-#\n\nNPC: greetings adventurer! how can I help you?\nAdventurer:"
+
+# get back story, and add it to the prompt
+with open("backstory.txt", "r") as input_file:
+    backstory = input_file.read()
+prompt = backstory + "\nThe following is a conversation between an Adventurer and an NPC in the Divine City.\n"
 
 # initial greeting
-greeting = start_sequence + ' greetings adventurer! how can i help you?'
+greeting = start_sequence + " greetings adventurer! how can i help you?"
 print(greeting)
-prompt += (greeting)
+prompt += greeting
 
 while True:
     # get user message
     message = input("Adventurer: ")
-    prompt+= (restart_sequence + message + start_sequence)
+    prompt += restart_sequence + message + start_sequence
 
     # get response from api
     response = openai.Completion.create(
@@ -27,10 +30,10 @@ while True:
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
-        stop=["\n", " NPC:", " Adventurer:"]
+        stop=["\n", " NPC:", " Adventurer:"],
     )
-    npc_message = response['choices'][0]['text']
+    npc_message = response["choices"][0]["text"]
     print(f"NPC:{npc_message}")
 
     # add response to prompt
-    prompt+= (npc_message + restart_sequence)
+    prompt += npc_message + restart_sequence
